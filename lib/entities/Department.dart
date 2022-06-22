5f1b98f67ec8850e5c24d26c79cf2018956f1fac
Marcelo Lupatini <mar.lupatini@gmail.com>
2022-06-21 23:42:48 -0300

    Modelos das 3 entidades do app

---
+++ lib/entities/Department.dart
@@ -0,0 +1,16 @@
+class Department {
+  int id;
+  String name;
+
+  Department({
+    required this.id,
+    required this.name,
+  });
+
+  factory Department.fromJson(Map<String, dynamic> json) {
+    return Department(
+      id: json["id"],
+      name: json['name'],
+    );
+  }
+}
---
+++ lib/entities/Patrimony.dart
@@ -0,0 +1,31 @@
+class Patrimony {
+  int id;
+  String name;
+  bool status;
+  String description;
+  String code;
+  String imgUrl;
+  double price;
+
+  Patrimony({
+    required this.id,
+    required this.name,
+    required this.status,
+    required this.description,
+    required this.code,
+    required this.imgUrl,
+    required this.price,
+  });
+
+  factory Patrimony.fromJson(Map<String, dynamic> json) {
+    return Patrimony(
+      id: json["id"],
+      name: json['name'],
+      status: json['status'],
+      description: json['description'],
+      code: json['code'],
+      imgUrl: json['imgUrl'],
+      price: json['price'],
+    );
+  }
+}
